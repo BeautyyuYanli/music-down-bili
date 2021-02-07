448c0d4f5a929222d140ec941834f84840528c6c
beautyyuyanli <beautyyuyanli@outlook.com>
2021-02-07 19:10:12 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import feedparser, requests, ffmpeg, time, re, codecs
 import downloader, cover
 # config
 proxies = { 'http': 'http://127.0.0.1:1081', 'https': 'http://127.0.0.1:1081'}
-rssurl = 'https://rsshub.app/bilibili/fav/10725385/53706285'
+# rssurl = 'https://rsshub.app/bilibili/fav/10725385/53706285'
+rssurl = 'https://rsshub.app/bilibili/fav/10725385/1173311485'
 # bv2av
 def bv2av(bvid):
     site = "https://api.bilibili.com/x/web-interface/view?bvid=" + bvid

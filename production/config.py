d858c82e73390639e563cd408a06f9cdbc6d9711
beautyyuyanli <beautyyuyanli@outlook.com>
2021-06-27 01:03:59 +0800

    change config to my own

--- production/config.py
+++ production/config.py
@@ -1,5 +1,5 @@
-uid = '438584984'
-fid = '1117924884'
+uid = '10725385'
+fid = '53706285'
 url = 'https://space.bilibili.com/' + uid + '/favlist?fid=' + fid + '&ftype=create'
 num_max = 10
 delayTime = 900
